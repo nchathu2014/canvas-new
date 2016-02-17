52ae6d4a3d03d216ea5900c654db068e5463dc2a
Chathuranga <UCHATNU@PEROOT.COM>
2016-02-17 20:19:46 +0530

    change the project strcuture

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Canvas from './components/Canvas';
+import Canvas from './components/canvas/Canvas';
 
 ReactDOM.render(
 	<Canvas/>,
